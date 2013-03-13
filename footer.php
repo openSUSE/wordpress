0cf1985a6f89081d8fb29073c07c2b760b460d3f
Ancor Gonzalez Sosa <ancor@suse.de>
2013-03-13 16:15:49 +0100

    Piwik tracking code added

--- footer.php
+++ footer.php
@@ -2,5 +2,19 @@
   <?php get_remote_snippet('footer'); ?>
   
   <?php wp_footer();?>
+
+  <!-- Piwik -->
+  <script type="text/javascript">
+    var pkBaseURL = (("https:" == document.location.protocol) ? "https://beans.opensuse.org/piwik/" : "http://beans.opensuse.org/piwik/");
+    document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+  </script><script type="text/javascript">
+    try {
+      var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 13);
+      piwikTracker.trackPageView();
+      piwikTracker.enableLinkTracking();
+    } catch( err ) {}
+  </script><noscript><p><img src="http://beans.opensuse.org/piwik/piwik.php?idsite=13" style="border:0" alt="" /></p></noscript>
+  <!-- End Piwik Tracking Code -->
+
 </body>
 </html>
